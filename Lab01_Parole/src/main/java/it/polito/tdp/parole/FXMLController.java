d739eb83ce747d7b8da369e2604fb0ad217c3f56
claudiazingarello <s238072@studenti.polito.it>
2020-03-13 12:16:27 +0100

    Esercizio 1 completato

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -3,6 +3,8 @@ package it.polito.tdp.parole;
 import it.polito.tdp.parole.model.Parole;
 
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -31,25 +33,49 @@ public class FXMLController {
 
     @FXML
     private Button btnReset;
+    
+    @FXML
+    private TextArea txtPrestazioni;
 
     @FXML
     void doInsert(ActionEvent event) {
     	
+    	//Pulisco l'area del risultato
     	txtResult.clear();
+    	
+    	//Prendo la parola inserita dalla casella
     	String parolaInserita = txtParola.getText();
     	parolaInserita = parolaInserita.trim();
-    		if (parolaInserita.length() == 0)
-    			elenco.addParola(parolaInserita);
-
-    		else {
-    			txtResult.appendText("Errone nell'inserimento");
-    		}
     	
+    		if (parolaInserita.length() != 0) {
+    			//Se la parola inserita e' valida, si inserisce nell'elenco
+    			elenco.addParola(parolaInserita);
+    			String risultato = "";
+    			for (String p : elenco.getElenco()) {
+    				risultato += p + "\n";
+    			}
+    			
+    			txtResult.setText(risultato);
+    			
+    		}
+    		else 
+    			txtResult.appendText("Errore nell'inserimento");
+    			
+    		
+    		txtParola.clear();
     }
 
+   
+    @FXML
+    void doCancella(ActionEvent event) {
+
+    }
+    
     @FXML
     void doReset(ActionEvent event) {
-    	// TODO
+    	elenco.reset();
+    	txtParola.clear();
+    	txtResult.clear();
     }
 
     @FXML
@@ -57,6 +83,7 @@ public class FXMLController {
         assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtPrestazioni != null : "fx:id=\"txtPrestazioni\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
 
         elenco = new Parole() ;
